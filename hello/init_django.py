223d65dd6f7f222d8f5e08be9d967132878e795f
swpper <haoshuang@xxx.com>
2023-09-27 00:38:55 +0800

    update

---
+++ hello/init_django.py
@@ -0,0 +1,25 @@
+"""
+初始化 Django 环境
+Calling django.setup() is required for “standalone” Django usage
+"""
+
+import sys
+import os
+import django
+
+
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hello.settings'
+django.setup()
+
+print(f'django_version={django.get_version()}')
+
+
+
+    
+
+
+if __name__ == '__main__':
+    ...    
+
--- mysite/oauth2jwt/utils.py
+++
@@ -1,8 +0,0 @@
-import jwt
-
-encoded_jwt = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
-print(encoded_jwt)
-
-decoded_jwt = jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
-print(decoded_jwt)
-
